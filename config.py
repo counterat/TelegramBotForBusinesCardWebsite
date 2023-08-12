3f6ab8ab8c56e0599a47cfbbeceea03149b9e999
Yuriy Zholtov <125594698+counterat@users.noreply.github.com>
2023-08-12 18:04:17 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+token_access = 'dkovsk0t94ri'
+token = '6130133980:AAHlH9S2HoVqqm7aLmLG_TUCio_2CvAP3Nc'
+cloud_name = "du73oow82"
+api_key="776475952999713"
+api_secret="ZsEgGp0xgT1wmuBc3rB7KSZLen8"
